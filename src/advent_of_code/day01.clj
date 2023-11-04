e896f952a57fd1ced8bfed86374a582577234b20
Randy J. Ray <rjray@blackperl.com>
2023-11-03 18:49:53 -0700

    Day 1

--- src/advent_of_code/day01.clj
+++ src/advent_of_code/day01.clj
@@ -3,9 +3,20 @@
 (defn part-1
   "Day 01 Part 1"
   [input]
-  input)
+  (loop [[ch & chs] input, floor 0]
+    (cond
+      (nil? ch) floor
+      (= ch \() (recur chs (inc floor))
+      (= ch \)) (recur chs (dec floor))
+      :else     "error")))
 
 (defn part-2
   "Day 01 Part 2"
   [input]
-  input)
+  (loop [[ch & chs] input, floor 0, idx 0]
+    (cond
+      (nil? ch)   floor
+      (< floor 0) idx
+      (= ch \()   (recur chs (inc floor) (inc idx))
+      (= ch \))   (recur chs (dec floor) (inc idx))
+      :else       "error")))
